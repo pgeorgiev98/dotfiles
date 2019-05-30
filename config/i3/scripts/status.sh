8fa331484f1c265ecfde7c65aee61656fe53c98f
Petko Georgiev <petko.vas.georgiev@gmail.com>
2019-05-30 21:23:59 +0300

    status.sh: Add JSON output and colors

--- config/i3/scripts/status.sh
+++ config/i3/scripts/status.sh
@@ -1,43 +1,73 @@
 #!/bin/bash
 
+ICON_SPACING=9
+
 SLEEPPID=''
 
 trap 'if [ ! -z $SLEEPPID ]; then kill -SIGINT $SLEEPPID; fi' SIGUSR1
 
 function date_time {
-	echo "  $(date +%Y-%m-%d)    $(date +%H:%M:%S)"
+	echo "{\"name\":\"date\",\"full_text\":\" $(date +%Y-%m-%d)\"},"
+	echo "{\"name\":\"time\",\"full_text\":\" $(date +%H:%M:%S)\"}"
 }
 
 function battery {
+	color=''
 	battery_level="$(cat /sys/class/power_supply/BAT0/capacity)"
-	battery_icon=''
-	if [ "$battery_level" -lt 15 ]; then
-		battery_icon=''
-	elif [ "$battery_level" -lt 40 ]; then
-		battery_icon=''
-	elif [ "$battery_level" -lt 65 ]; then
-		battery_icon=''
-	elif [ "$battery_level" -lt 90 ]; then
-		battery_icon=''
+	if [ $? -eq 0 ]; then
+		if [ "$battery_level" -lt 15 ]; then
+			battery_icon=''
+			color=${COLOR_RED}
+		elif [ "$battery_level" -lt 40 ]; then
+			battery_icon=''
+			color=${COLOR_YELLOW}
+		elif [ "$battery_level" -lt 65 ]; then
+			battery_icon=''
+			color=${COLOR_YELLOW}
+		elif [ "$battery_level" -lt 90 ]; then
+			battery_icon=''
+			color=${COLOR_GREEN}
+		else
+			battery_icon=''
+			color=${COLOR_GREEN}
+		fi
+		battery_level="$battery_level%"
 	else
-		battery_icon=''
+		battery_icon=''
+		battery_level="N/A"
+		color=${COLOR_RED}
 	fi
-	echo "$battery_icon  $battery_level%"
+
+	if [ -n "$color" ]; then
+		color=",\"color\":\"$color\""
+	fi
+
+	echo "{\"name\":\"battery_icon\",\"full_text\":\"$battery_icon\",\"separator\":false,\"separator_block_width\":${ICON_SPACING}${color}},"
+	echo "{\"name\":\"battery_text\",\"full_text\":\"$battery_level\",\"min_width\":\"100%\",\"align\":\"right\"${color}}"
 }
 
 function volume {
+	color=''
 	sink=$(pactl list short sinks | sed -e 's,^\([0-9][0-9]*\)[^0-9].*,\1,' | head -n 1 )
 	level=$(pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(( $sink+ 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,' )
 	muted=$(pactl list sinks | grep '^[[:space:]]Mute:' | head -n $((sink+1)) | tail -n 1 | awk '{printf $2}')
 	volume_icon=''
 	if [ "$muted" == "yes" ]; then
-		volume_icon=''
-	elif [ "$level" -lt 50 ]; then
+		volume_icon=''
+	elif [ "$level" -lt 34 ]; then
+		volume_icon=''
+	elif [ "$level" -lt 67 ]; then
 		volume_icon=''
 	else
 		volume_icon=''
 	fi
-	echo "$volume_icon  $level%"
+
+	if [ "$level" -gt 100 ]; then
+		color=",\"color\":\"${COLOR_RED}\""
+	fi
+
+	echo "{\"name\":\"volume_icon\",\"full_text\":\"$volume_icon\",\"min_width\":\"\",\"separator\":false,\"separator_block_width\":${ICON_SPACING}${color}},"
+	echo "{\"name\":\"volume_text\",\"full_text\":\"$level%\",\"min_width\":\"100%\",\"align\":\"right\"${color}}"
 }
 
 function cpu {
@@ -82,9 +112,20 @@ function cpu {
 	done
 	usage_text="$usage_text ]"
 
+	temp_color=''
 	temp="$(cat /sys/class/hwmon/hwmon1/temp1_input)"
 	temp=$((temp/1000))
-	echo "  $temp °C $usage_text"
+
+	if [ "$temp" -ge 80 ]; then
+		temp_color=",\"color\":\"$COLOR_RED\""
+	elif [ "$temp" -ge 65 ]; then
+		temp_color=",\"color\":\"$COLOR_YELLOW\""
+	fi
+
+	echo "{\"name\":\"cpu_temp_icon\",\"full_text\":\"\",\"separator\":false,\"separator_block_width\":${ICON_SPACING}${temp_color}},"
+	echo "{\"name\":\"cpu_temp_text\",\"full_text\":\"$temp °C\",\"min_width\":\"100 °C\",\"align\":\"right\"${temp_color}},"
+
+	echo "{\"name\":\"cpu_usage_text\",\"full_text\":\"$usage_text\"}"
 }
 
 function pretty_size {
@@ -98,17 +139,16 @@ function pretty_size {
 	else
 		str=$(printf '%.1f G' `bc -l <<< "$v/(1024*1024*1024)"`)
 	fi
-	pad=$(printf '%*s' $((7 - ${#str})) '' | sed 's/ /_/g')
-	printf "${pad}${str}"
+	printf "${str}"
 }
 
 function wifi {
 	IFACE=wlo1
 	if [ $(grep -c '<.*UP.*>' <<< `ip link show dev wlo1`) -eq 0 ]; then
-		echo " Down"
+		echo "{\"name\":\"wifi\",\"full_text\":\" Down\",\"color\":\"$COLOR_RED\"}"
 		exit 0
 	elif [ $(grep -c 'state DOWN' <<< `ip link show dev wlo1`) -ne 0 ]; then
-		echo " Disconnected"
+		echo "{\"name\":\"wifi\",\"full_text\":\" Disconnected\",\"color\":\"$COLOR_RED\"}"
 		exit 0
 	fi
 
@@ -116,13 +156,6 @@ function wifi {
 	iwconfig $IFACE > $TMPFILE
 	SSID=`grep -o 'ESSID:".*"' $TMPFILE`
 	SSID="${SSID:7:-1}"
-	SIGNAL=`grep -o 'Signal level=-[0-9]* dBm' $TMPFILE`
-	SIGNAL=`echo "2*(${SIGNAL:13:-4}+100)" | bc -l`
-	if [ $SIGNAL -gt 99 ]; then
-		SIGNAL=99
-	elif [ $SIGNAL -lt 0 ]; then
-		SIGNAL=0
-	fi
 	IP=`ip addr show dev wlo1 | grep -o 'inet [0-9]*\.[0-9]*\.[0-9]*\.[0-9]*'`
 	IP=${IP:5}
 	rm -f $TMPFILE
@@ -135,17 +168,43 @@ function wifi {
 	drx=$((rx-old_rx))
 	dtx=$((tx-old_tx))
 
-	echo " ($SIGNAL% at $SSID) $IP  $(pretty_size $drx)  $(pretty_size $dtx)"
+	no_sep="\"separator\":false,\"separator_block_width\":${ICON_SPACING}"
+
+	echo "{\"name\":\"wifi_name\",\"full_text\":\" $SSID\",$no_sep},"
+	echo "{\"name\":\"wifi_ip\",\"full_text\":\"$IP\",$no_sep},"
+	echo "{\"name\":\"wifi_down_icon\",\"full_text\":\"\",$no_sep},"
+	echo "{\"name\":\"wifi_down_text\",\"full_text\":\"$(pretty_size $drx)\",\"min_width\":\"999.9 M\",\"align\":\"right\",$no_sep},"
+	echo "{\"name\":\"wifi_up_icon\",\"full_text\":\"\",$no_sep},"
+	echo "{\"name\":\"wifi_up_text\",\"full_text\":\"$(pretty_size $dtx)\",\"min_width\":\"999.9 M\",\"align\":\"right\"}"
 }
 
 function ram {
-	MEM=$(free | head -n 2 | tail -n 1 | awk '{printf $3+$5}')
-	MEM=$((MEM/1024))
-	echo "  ${MEM}M "
+	used=$(free | head -n 2 | tail -n 1 | awk '{printf $3+$5}')
+	total=$(free | head -n 2 | tail -n 1 | awk '{printf $2}')
+	if [ $(bc <<< "$used > $total * 0.8") -ne 0 ]; then
+		color=",\"color\":\"$COLOR_RED\""
+	else
+		color=''
+	fi
+	used=$((used/1024))
+	total=$((total/1024))
+	echo "{\"name\":\"ram_icon\",\"full_text\":\"\",\"separator\":false,\"separator_block_width\":${ICON_SPACING}${color}},"
+	echo "{\"name\":\"ram_text\",\"full_text\":\"${used}M\",\"min_width\":\"${total}M\",\"align\":\"right\"${color}}"
 }
 
+echo '{ "version": 1 }'
+echo '['
+echo '[]'
 while true; do
-	echo "$(ram) $(cpu)  $(wifi)  $(volume)  $(battery)  $(date_time) " || exit 1
+	COLOR_FG=$(     xrdb -q | grep -m 1 foreground: | awk '{print $2}')
+	COLOR_RED=$(    xrdb -q | grep -m 1 color1:     | awk '{print $2}')
+	COLOR_GREEN=$(  xrdb -q | grep -m 1 color2:     | awk '{print $2}')
+	COLOR_YELLOW=$( xrdb -q | grep -m 1 color3:     | awk '{print $2}')
+	COLOR_BLUE=$(   xrdb -q | grep -m 1 color4:     | awk '{print $2}')
+	COLOR_MAGENTA=$(xrdb -q | grep -m 1 color5:     | awk '{print $2}')
+	COLOR_CYAN=$(   xrdb -q | grep -m 1 color6:     | awk '{print $2}')
+
+	echo ",[$(ram),$(cpu),$(wifi),$(volume),$(battery),$(date_time)]" || exit 1
 	sleep 1& &>/dev/null
 	SLEEPPID=$!
 	wait
